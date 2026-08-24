d4c8cde25fd7fd9c8e792c85e1b42d6e404b79fb
FazaViranza <201728547+FazaViranza@users.noreply.github.com>
2026-08-24 18:13:49 +0700

    C7
    Configure Supabase database connection for production

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 const { Pool } = require("pg");
+
 require("dotenv").config();
 
 const pool = new Pool({
@@ -7,6 +8,10 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 pool.on("connect", () => {
